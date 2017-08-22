cc89bd2aa8adad8c01d9c44e9ae7de51516a5de3
Vladimir Pasquier <vpasquier@nuxeo.com>
2017-08-21 23:54:54 -0400

    GH-1: finalize lambda

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var config = require("./config.json");
-var request = require("request");
+const config = require("./config.json");
+const request = require("request");
 
 exports.handler = function (event, context) {
+    "use strict";
     console.log("Start Lambda - Payload content:", event);
 
     if (event.token !== config.token) {
         return context.fail("Unauthorized request. This token is not valid:" + config.token);
     }
 
-    console.log("Slack Command Token is correct.");
-
     request("https://slack.com/api/users.list?token=" + config.oauthToken, function (error, response, body) {
-        console.log("Returning from the Slack member list access call.");
-
-        if (error || !response) {
-            return context.fail("Cannot access to Slack team - " + error);
-        } else {
+        if (!(error || !response)) {
             if (response && response.statusCode !== 200) {
                 return context.fail("Cannot access to Slack team users - Code:" + response.statusCode + " - " + response);
             }
+        } else {
+            return context.fail("Cannot access to Slack team - " + error);
         }
 
         let json = JSON.parse(body);
@@ -27,29 +24,39 @@ exports.handler = function (event, context) {
             context.fail("Cannot access to Slack team - " + json.error);
         }
 
-        let status = {};
-        status.users = [];
+        let statusList = [];
 
-        for (var i = 0; i < json.members.length; i++) {
-            // "status_text": "NYC",
-            // "status_emoji": ":flag-us:",
-            if (!json.members[i].profile.is_bot && json.members[i].profile.real_name !== "slackbot") {
-                console.log(json.members[i].profile.status_emoji);
-                status.id = json.members[i].profile.status_emoji;
-                console.log(status);
-                status.users.push(json.members[i].profile.name + " - " + json.members[i].profile.status_text);
-                console.log(status);
+        json.members.forEach(function (member) {
+            let profile = member.profile;
+            let bot = profile.is_bot || profile.bot_id;
+            if (!bot && profile.real_name !== "slackbot") {
+                let id = profile.status_emoji;
+                if (!id) {
+                    id = ":earth_africa:";
+                }
+                if (!statusList[id]) {
+                    statusList[id] = [];
+                }
+                if (profile.status_text) {
+                    statusList[id].push(profile.real_name + " - " + profile.status_text);
+                } else {
+                    statusList[id].push(profile.real_name);
+                }
             }
-        }
+        });
 
         let responseBody = {};
-        responseBody.text = "People Location\n";
-        for (var i = 0; i < status.length; i++) {
-            responseBody.text = status[i].id + "\n";
-            for (var j = 0; j < status[i].users.length; j++) {
-                responseBody.text = status[i].users[j] + "\n";
-            }
+        responseBody.text = "";
+        for (var id in statusList) {
+            responseBody.text = responseBody.text + id + "\n";
+            statusList[id].forEach(function (user) {
+                responseBody.text = responseBody.text + user + "\n";
+            });
+            responseBody.text = responseBody.text + "\n";
         }
+
+        // For unit tests purpose
+        event.result = responseBody.text;
 
         context.succeed(responseBody);
     });
