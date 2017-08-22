838bc02dec794751b0552a1ce72f45f060b99a86
Vladimir Pasquier <vpasquier@nuxeo.com>
2017-08-21 23:55:17 -0400

    GH-1: finalize jasmine unit tests - add reporter

--- spec/lambda.spec.js
+++ spec/lambda.spec.js
@@ -1,33 +1,56 @@
-var proxyquire = require('proxyquire');
-var config = require('./config-test.json');
+const proxyquire = require("proxyquire");
+const config = require("./config-test.json");
+const timeout = 3000;
 
-describe('Lambda Slack Location', function () {
+describe("Lambda Slack Location", function () {
     let lambda, context;
 
-    beforeEach(function () {
-        lambda = proxyquire('../index', {});
-        context = jasmine.createSpyObj('context', ['done', 'succeed', 'fail']);
+    beforeAll(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     });
 
-    it('should return a token check failure', function () {
+    beforeEach(function (done) {
+        lambda = proxyquire("../index", {});
+        context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
         let evnt = {
-            token: 'fail'
+            token: "fail"
         };
-        waitsFor(lambda.handler(evnt, context),"The Ajax call timed out.",5000);
-        runs(function () {
-            expect(context.fail).toHaveBeenCalled();
-            expect(context.succeed).not.toHaveBeenCalled();
-        });
+        lambda.handler(evnt, context);
+        setTimeout(function () {
+            done();
+        }, timeout);
+    });
+    it("should return a token check failure", function (done) {
+        expect(context.fail).toHaveBeenCalled();
+        expect(context.succeed).not.toHaveBeenCalled();
+        done();
     });
 
-    it('should succeed', function () {
-        let evnt = {
-            token: config.token
-        };
-        waitsFor(lambda.handler(evnt, context),"The Ajax call timed out.",5000);
-        runs(function () {
-            expect(context.fail).toHaveBeenCalled();
-            expect(context.succeed).not.toHaveBeenCalled();
-        });
+});
+
+describe("Lambda Slack Location", function () {
+    let lambda, context;
+    let evnt = {
+        token: config.token,
+        result: ""
+    };
+
+    beforeAll(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    });
+
+    beforeEach(function (done) {
+        lambda = proxyquire("../index", {});
+        context = jasmine.createSpyObj("context", ["done", "succeed", "fail"]);
+        lambda.handler(evnt, context);
+        setTimeout(function () {
+            done();
+        }, timeout);
+    });
+    it("should succeed", function (done) {
+        expect(context.succeed).toHaveBeenCalled();
+        expect(context.fail).not.toHaveBeenCalled();
+        expect(evnt.result).toEqual(":flag-us:\nvlad letter - NYC\n\n");
+        done();
     });
 });
